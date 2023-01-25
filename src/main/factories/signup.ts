d1bd42fc45d9e59a50c01c79b5255fdd610c8528
jefferson guirra <jeffersonguirra8@gmail.com>
2023-01-25 14:35:17 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
