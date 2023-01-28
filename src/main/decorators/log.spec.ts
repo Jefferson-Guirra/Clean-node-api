eeaf7421adf96f540e029382d1169d69d24b5148
jefferson guirra <jeffersonguirra8@gmail.com>
2023-01-28 19:00:48 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,25 +1,38 @@
 import { LogControllerDecorator } from './log'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
-
+interface sutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse | undefined> {
+      const fakeAccount: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        }
+      }
+      return await Promise.resolve(fakeAccount)
+    }
+  }
+  return new ControllerStub()
+}
+const makeSut = (): sutTypes => {
+  const controllerStub = makeControllerStub()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    controllerStub,
+    sut
+  }
+}
 describe('Log Controller Decorator', () => {
   test('Should call controller handler', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse | undefined> {
-        const fakeAccount: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'any_name',
-            email: 'any_email@mail.com',
-            password: 'any_password',
-            passwordConfirmation: 'any_password'
-          }
-        }
-        return await Promise.resolve(fakeAccount)
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest: HttpRequest = {
       body: {
         name: 'any_name',
