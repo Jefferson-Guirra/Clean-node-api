a0feda4ca4600c296a1f581766ca7472c28bef26
jefferson guirra <jeffersonguirra8@gmail.com>
2023-01-28 17:06:00 -0300

    feat: ensure mongodb recconect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,7 +6,7 @@ import { ObjectId } from 'mongodb'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection?.insertOne(accountData)
     const account = await accountCollection?.findOne({ _id: new ObjectId(result?.insertedId) })
     return await new Promise(resolve => {
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,16 +1,23 @@
-import { MongoClient } from 'mongodb'
+import { Collection, Document, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient | null,
   db: null as MongoClient | null,
+  uri: null as string | null,
+
   async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   },
   async disconnect () {
     await this.client?.close()
+    this.client = null
   },
-  getCollection (name: string) {
-    return this.client?.db().collection(name)
+  async getCollection (name: string): Promise<Collection<Document> > {
+    if (this.client === null) {
+      await this.connect(this.uri as string)
+    }
+    return this.client?.db().collection(name) as Collection<Document>
   },
   map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
