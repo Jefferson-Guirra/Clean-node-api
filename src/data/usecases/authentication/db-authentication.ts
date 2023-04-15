6dac537c034e64171c5f31cdb5f0f914d97104f6
jefferson guirra <jeffersonguirra8@gmail.com>
2023-04-15 16:15:56 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,13 +1,16 @@
 import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication'
+import { HashComparer } from '../../protocols/criptography/hash-compare'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 export class DbAuthentication implements Authentication {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account != null) {
+      await this.hashComparer.compare(authentication.password, account.password)
       return await Promise.resolve('access_token')
     } else {
       return null
