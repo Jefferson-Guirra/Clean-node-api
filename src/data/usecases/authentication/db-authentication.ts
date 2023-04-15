7bdb5fb4a02d1505b251fc5b42c57abd207b72f2
jefferson guirra <jeffersonguirra8@gmail.com>
2023-04-15 15:41:17 -0300

    feat: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -6,7 +6,11 @@ export class DbAuthentication implements Authentication {
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
-    await this.loadAccountByEmailRepository.load(authentication.email)
-    return await Promise.resolve('access_token')
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (account != null) {
+      return await Promise.resolve('access_token')
+    } else {
+      return null
+    }
   }
 }
