0a49b0a02e8ff09f685eaf4474594bc0c9e4ee2c
jefferson guirra <jeffersonguirra8@gmail.com>
2023-05-16 23:12:33 -0300

    test: ensure AccountMongoRepository return null if loadByEmail null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -45,4 +45,10 @@ describe('Account Mongo Repository', () => {
     expect(account.password).toBe('any_password')
     expect(account.email).toBe('any_email@mail.com')
   })
+
+  test('should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
+  })
 })
